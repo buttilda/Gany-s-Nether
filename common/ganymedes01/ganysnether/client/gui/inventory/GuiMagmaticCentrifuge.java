de061dfe2e296b0c59bb42c66fcadf80e6dbeeae
ganymedes01 <foka_12@hotmail.com>
2013-12-24 23:09:25 +0000

    Renaming stuff

--- common/ganymedes01/ganysnether/client/gui/inventory/GuiMagmaticCentrifuge.java
+++ common/ganymedes01/ganysnether/client/gui/inventory/GuiMagmaticCentrifuge.java
@@ -45,17 +45,17 @@ public class GuiMagmaticCentrifuge extends GuiGanysNether {
 	protected void drawGuiContainerBackgroundLayer(float f, int mouseX, int mouseY) {
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		mc.renderEngine.bindTexture(new ResourceLocation(Utils.getGUITexture(Strings.MAGMATIC_CENTRIFUGE_NAME)));
-		int j = (width - xSize) / 2;
-		int k = (height - ySize) / 2;
+		int x = (width - xSize) / 2;
+		int y = (height - ySize) / 2;
 
-		drawTexturedModalRect(j, k, 0, 0, xSize, ySize);
+		drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
 
-		drawTexturedModalRect(j + ((Slot) container.inventorySlots.get(2)).xDisplayPosition - 1, k + ((Slot) container.inventorySlots.get(2)).yDisplayPosition - 1, 176, 3, 18, 18);
-		drawTexturedModalRect(j + ((Slot) container.inventorySlots.get(3)).xDisplayPosition - 1, k + ((Slot) container.inventorySlots.get(3)).yDisplayPosition - 1, 176, 3, 18, 18);
-		displayGauge(FluidRegistry.LAVA, j, k, 155, 53, centrifuge.getScaledFluidAmount(52));
+		drawTexturedModalRect(x + ((Slot) container.inventorySlots.get(2)).xDisplayPosition - 1, y + ((Slot) container.inventorySlots.get(2)).yDisplayPosition - 1, 176, 3, 18, 18);
+		drawTexturedModalRect(x + ((Slot) container.inventorySlots.get(3)).xDisplayPosition - 1, y + ((Slot) container.inventorySlots.get(3)).yDisplayPosition - 1, 176, 3, 18, 18);
+		displayGauge(FluidRegistry.LAVA, x, y, 155, 53, centrifuge.getScaledFluidAmount(52));
 
 		mc.renderEngine.bindTexture(new ResourceLocation(Utils.getGUITexture(Strings.MAGMATIC_CENTRIFUGE_NAME)));
-		drawTexturedModalRect(j + 155, k + 53, 176, 21, 16, 52);
+		drawTexturedModalRect(x + 155, y + 53, 176, 21, 16, 52);
 	}
 
 	@Override
