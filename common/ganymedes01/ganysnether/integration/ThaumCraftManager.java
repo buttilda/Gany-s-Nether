19d42a85c199c2afa04cb0abc3db2a8eb1b0bfd5
ganymedes01 <foka_12@hotmail.com>
2014-02-13 00:50:17 +0000

    Fix for crash when registering aspects with undertakers turned off

--- common/ganymedes01/ganysnether/integration/ThaumCraftManager.java
+++ common/ganymedes01/ganysnether/integration/ThaumCraftManager.java
@@ -3,6 +3,7 @@ package ganymedes01.ganysnether.integration;
 import ganymedes01.ganysnether.blocks.ModBlocks;
 import ganymedes01.ganysnether.core.utils.HoeList;
 import ganymedes01.ganysnether.items.ModItems;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import thaumcraft.api.ItemApi;
@@ -29,35 +30,35 @@ public class ThaumCraftManager extends Integration {
 
 		FMLInterModComms.sendMessage("Thaumcraft", "harvestClickableCrop", new ItemStack(ModBlocks.witherShrub, 1, 8));
 
-		addAspectsToItem(ModBlocks.soulGlass.blockID, new Aspect[] { Aspect.CRYSTAL, Aspect.SOUL }, new int[] { 1, 1 });
-		addAspectsToItem(ModBlocks.glowBox.blockID, new Aspect[] { Aspect.LIGHT, Aspect.SENSES }, new int[] { 3, 1 });
-		addAspectsToItem(ModBlocks.colouredQuartzBlock.blockID, new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 1, 1 });
-		addAspectsToItem(ModBlocks.colouredChiselledQuartzBlock.blockID, new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 1, 1 });
+		addAspectsToItem(ModBlocks.soulGlass, new Aspect[] { Aspect.CRYSTAL, Aspect.SOUL }, new int[] { 1, 1 });
+		addAspectsToItem(ModBlocks.glowBox, new Aspect[] { Aspect.LIGHT, Aspect.SENSES }, new int[] { 3, 1 });
+		addAspectsToItem(ModBlocks.colouredQuartzBlock, new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 1, 1 });
+		addAspectsToItem(ModBlocks.colouredChiselledQuartzBlock, new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 1, 1 });
 		for (int i = 0; i < ModBlocks.colouredQuartzPillar.length; i++)
-			addAspectsToItem(ModBlocks.colouredQuartzPillar[i].blockID, new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 1, 1 });
+			addAspectsToItem(ModBlocks.colouredQuartzPillar[i], new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 1, 1 });
 		for (int i = 0; i < ModBlocks.colouredQuartzBlockStairs.length; i++)
-			addAspectsToItem(ModBlocks.colouredQuartzBlockStairs[i].blockID, new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 2, 2 });
-		addAspectsToItem(ModBlocks.undertaker.blockID, new Aspect[] { Aspect.EARTH, Aspect.TRAP, Aspect.SOUL, Aspect.CRYSTAL, Aspect.ENERGY }, new int[] { 7, 7, 7, 1, 1 });
-		addAspectsToItem(ModItems.blazeIngot.itemID, 1, new Aspect[] { Aspect.FIRE, Aspect.MAGIC, Aspect.METAL }, new int[] { 2, 2, 2 });
-		addAspectsToItem(ModItems.quarzBerrySeeds.itemID, new Aspect[] { Aspect.SEED, Aspect.UNDEAD }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.ghostSeeds.itemID, new Aspect[] { Aspect.SEED, Aspect.SOUL }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.witherShrubSeeds.itemID, new Aspect[] { Aspect.SEED, Aspect.UNDEAD }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.batWing.itemID, new Aspect[] { Aspect.AIR, Aspect.FLIGHT }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.cookedBatWing.itemID, new Aspect[] { Aspect.AIR, Aspect.HUNGER }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.wolfTeeth.itemID, new Aspect[] { Aspect.BEAST }, new int[] { 1 });
-		addAspectsToItem(ModItems.silverfishScale.itemID, new Aspect[] { Aspect.BEAST, Aspect.EARTH }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.glowingReed.itemID, new Aspect[] { Aspect.AIR, Aspect.FIRE, Aspect.LIGHT }, new int[] { 1, 1, 1 });
-		addAspectsToItem(ModItems.spectreWheat.itemID, new Aspect[] { Aspect.CROP, Aspect.SOUL }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.quarzBerry.itemID, new Aspect[] { Aspect.CROP, Aspect.CRYSTAL }, new int[] { 1, 1 });
-		addAspectsToItem(Item.monsterPlacer.itemID, new Aspect[] { Aspect.SOUL, Aspect.TRAP }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.skeletonSpawner.itemID, new Aspect[] { Aspect.SOUL, Aspect.TRAP }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.spookyFlour.itemID, new Aspect[] { Aspect.SOUL, Aspect.CROP }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.dimensionalBread.itemID, new Aspect[] { Aspect.SOUL, Aspect.CROP, Aspect.HUNGER }, new int[] { 1, 3, 2 });
-		addAspectsToItem(ModItems.blazeIngot.itemID, 2, new Aspect[] { Aspect.FIRE }, new int[] { 1 });
-		addAspectsToItem(ModItems.flour.itemID, new Aspect[] { Aspect.HUNGER, Aspect.ENTROPY }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.hellBushSeeds.itemID, new Aspect[] { Aspect.SEED, Aspect.FIRE }, new int[] { 1, 1 });
-		addAspectsToItem(ModItems.lavaBerry.itemID, new Aspect[] { Aspect.FIRE }, new int[] { 3 });
-		addAspectsToItem(ModBlocks.blazingCactoid.blockID, new Aspect[] { Aspect.PLANT, Aspect.ENTROPY, Aspect.FIRE }, new int[] { 3, 1, 1 });
+			addAspectsToItem(ModBlocks.colouredQuartzBlockStairs[i], new Aspect[] { Aspect.WATER, Aspect.CRYSTAL }, new int[] { 2, 2 });
+		addAspectsToItem(ModBlocks.undertaker, new Aspect[] { Aspect.EARTH, Aspect.TRAP, Aspect.SOUL, Aspect.CRYSTAL, Aspect.ENERGY }, new int[] { 7, 7, 7, 1, 1 });
+		addAspectsToItem(ModItems.blazeIngot, 1, new Aspect[] { Aspect.FIRE, Aspect.MAGIC, Aspect.METAL }, new int[] { 2, 2, 2 });
+		addAspectsToItem(ModItems.quarzBerrySeeds, new Aspect[] { Aspect.SEED, Aspect.UNDEAD }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.ghostSeeds, new Aspect[] { Aspect.SEED, Aspect.SOUL }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.witherShrubSeeds, new Aspect[] { Aspect.SEED, Aspect.UNDEAD }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.batWing, new Aspect[] { Aspect.AIR, Aspect.FLIGHT }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.cookedBatWing, new Aspect[] { Aspect.AIR, Aspect.HUNGER }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.wolfTeeth, new Aspect[] { Aspect.BEAST }, new int[] { 1 });
+		addAspectsToItem(ModItems.silverfishScale, new Aspect[] { Aspect.BEAST, Aspect.EARTH }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.glowingReed, new Aspect[] { Aspect.AIR, Aspect.FIRE, Aspect.LIGHT }, new int[] { 1, 1, 1 });
+		addAspectsToItem(ModItems.spectreWheat, new Aspect[] { Aspect.CROP, Aspect.SOUL }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.quarzBerry, new Aspect[] { Aspect.CROP, Aspect.CRYSTAL }, new int[] { 1, 1 });
+		addAspectsToItem(Item.monsterPlacer, new Aspect[] { Aspect.SOUL, Aspect.TRAP }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.skeletonSpawner, new Aspect[] { Aspect.SOUL, Aspect.TRAP }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.spookyFlour, new Aspect[] { Aspect.SOUL, Aspect.CROP }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.dimensionalBread, new Aspect[] { Aspect.SOUL, Aspect.CROP, Aspect.HUNGER }, new int[] { 1, 3, 2 });
+		addAspectsToItem(ModItems.blazeIngot, 2, new Aspect[] { Aspect.FIRE }, new int[] { 1 });
+		addAspectsToItem(ModItems.flour, new Aspect[] { Aspect.HUNGER, Aspect.ENTROPY }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.hellBushSeeds, new Aspect[] { Aspect.SEED, Aspect.FIRE }, new int[] { 1, 1 });
+		addAspectsToItem(ModItems.lavaBerry, new Aspect[] { Aspect.FIRE }, new int[] { 3 });
+		addAspectsToItem(ModBlocks.blazingCactoid, new Aspect[] { Aspect.PLANT, Aspect.ENTROPY, Aspect.FIRE }, new int[] { 3, 1, 1 });
 	}
 
 	@Override
@@ -69,8 +70,28 @@ public class ThaumCraftManager extends Integration {
 		return "Thaumcraft";
 	}
 
+	private void addAspectsToItem(Item item, Aspect[] aspects, int[] amounts) {
+		if (item != null)
+			addAspectsToItem(item.itemID, -1, aspects, amounts);
+	}
+
+	private void addAspectsToItem(Block block, Aspect[] aspects, int[] amounts) {
+		if (block != null)
+			addAspectsToItem(block.blockID, -1, aspects, amounts);
+	}
+
 	private void addAspectsToItem(int id, Aspect[] aspects, int[] amounts) {
 		addAspectsToItem(id, -1, aspects, amounts);
+	}
+
+	private void addAspectsToItem(Block block, int meta, Aspect[] aspects, int[] amounts) {
+		if (block != null)
+			addAspectsToItem(block.blockID, meta, aspects, amounts);
+	}
+
+	private void addAspectsToItem(Item item, int meta, Aspect[] aspects, int[] amounts) {
+		if (item != null)
+			addAspectsToItem(item.itemID, meta, aspects, amounts);
 	}
 
 	private void addAspectsToItem(int id, int meta, Aspect[] aspects, int[] amounts) {
