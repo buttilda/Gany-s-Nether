8943816f0d022fecf0521c32ef130135ed379b8b
ganymedes01 <foka_12@hotmail.com>
2014-09-30 17:36:24 +0100

    Don't spawn the undertaker is keepInventory is on

--- src/main/java/ganymedes01/ganysnether/core/handlers/EntityEvents.java
+++ src/main/java/ganymedes01/ganysnether/core/handlers/EntityEvents.java
@@ -17,6 +17,7 @@ import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import cpw.mods.fml.common.eventhandler.EventPriority;
@@ -33,10 +34,11 @@ public class EntityEvents {
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void deathEvent(LivingDeathEvent event) {
-		if (event.entityLiving.worldObj.isRemote)
+		World world = event.entityLiving.worldObj;
+		if (world.isRemote)
 			return;
 
-		if (event.entityLiving instanceof EntityPlayer) {
+		if (event.entityLiving instanceof EntityPlayer && !world.getGameRules().getGameRuleBooleanValue("keepInventory")) {
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
 			if (!player.inventory.consumeInventoryItem(Item.getItemFromBlock(ModBlocks.undertaker)))
 				return;
@@ -44,10 +46,10 @@ public class EntityEvents {
 			int x = (int) player.posX;
 			int y = (int) player.posY;
 			int z = (int) player.posZ;
-			if (!player.worldObj.isAirBlock(x, y, z))
+			if (!world.isAirBlock(x, y, z))
 				y++;
-			player.worldObj.setBlock(x, y, z, ModBlocks.undertaker);
-			TileEntityUndertaker undertaker = Utils.getTileEntity(player.worldObj, x, y, z, TileEntityUndertaker.class);
+			world.setBlock(x, y, z, ModBlocks.undertaker);
+			TileEntityUndertaker undertaker = Utils.getTileEntity(world, x, y, z, TileEntityUndertaker.class);
 			if (undertaker != null) {
 				for (int i = 0; i < player.inventory.mainInventory.length; i++) {
 					ItemStack cont = player.inventory.mainInventory[i];
@@ -59,7 +61,7 @@ public class EntityEvents {
 				for (int i = 0; i < player.inventory.armorInventory.length; i++) {
 					ItemStack cont = player.inventory.armorInventory[player.inventory.armorInventory.length - 1 - i];
 					if (cont != null) {
-						undertaker.setInventorySlotContents(i + player.inventory.mainInventory.length, cont);
+						undertaker.setInventorySlotContents(i + player.inventory.mainInventory.length, cont.copy());
 						player.inventory.armorInventory[player.inventory.armorInventory.length - 1 - i] = null;
 					}
 				}
