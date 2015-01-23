7cd1fd36ea258c8a67a1c20e5add71cfb36eb0ee
ganymedes01 <foka_12@hotmail.com>
2015-01-23 03:20:17 -0200

    Forgot the weeping pods :3

--- src/main/java/ganymedes01/ganysnether/integration/MFRManager.java
+++ src/main/java/ganymedes01/ganysnether/integration/MFRManager.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntitySkull;
@@ -40,6 +41,41 @@ public class MFRManager extends Integration {
 		FactoryRegistry.sendMessage("registerPlantable", new PlantableSugarCane(Item.getItemFromBlock(ModBlocks.blazingCactoid), ModBlocks.blazingCactoid));
 		FactoryRegistry.sendMessage("registerPlantable", new PlantableSugarCane(ModItems.glowingReed, ModBlocks.glowingReed));
 		FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.witherShrubSeeds, ModBlocks.witherShrub));
+		FactoryRegistry.sendMessage("registerPlantable", new Plantable(Items.ghast_tear, ModBlocks.weepingPod) {
+
+			@Override
+			public boolean canBePlantedHere(World world, int x, int y, int z, ItemStack stack) {
+				for (ForgeDirection side : SIDES)
+					if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == Blocks.obsidian)
+						return true;
+				return false;
+			}
+
+			@Override
+			public ReplacementBlock getPlantedBlock(World world, int x, int y, int z, ItemStack stack) {
+				ReplacementBlock block = super.getPlantedBlock(world, x, y, z, stack);
+				for (ForgeDirection side : SIDES)
+					if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == Blocks.obsidian)
+						switch (side.getOpposite().ordinal()) {
+							case 2:
+								block.setMeta(0);
+								break;
+							case 3:
+								block.setMeta(2);
+								break;
+							case 4:
+								block.setMeta(3);
+								break;
+							case 5:
+								block.setMeta(1);
+								break;
+							default:
+								break;
+						}
+
+				return block;
+			}
+		});
 
 		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.spectreWheat));
 		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.quarzBerryBush));
@@ -56,6 +92,13 @@ public class MFRManager extends Integration {
 						return tile != null && tile.func_145904_a() == 1;
 					}
 				return false;
+			}
+		});
+
+		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.weepingPod) {
+			@Override
+			public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z) {
+				return (world.getBlockMetadata(x, y, z) & 12) >> 2 >= 2;
 			}
 		});
 	}
