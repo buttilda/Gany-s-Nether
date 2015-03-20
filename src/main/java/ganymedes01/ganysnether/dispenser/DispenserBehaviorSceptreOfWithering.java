ab5e06b847fc7392daa0574ceb9bd699e4f0163e
ganymedes01 <foka_12@hotmail.com>
2015-03-20 19:01:12 -0300

    Sceptre of Withering now shoots wither skulls

--- src/main/java/ganymedes01/ganysnether/dispenser/DispenserBehaviorSceptreOfWithering.java
+++ src/main/java/ganymedes01/ganysnether/dispenser/DispenserBehaviorSceptreOfWithering.java
@@ -1,8 +1,16 @@
 package ganymedes01.ganysnether.dispenser;
 
+import net.minecraft.block.BlockDispenser;
 import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
 import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityWitherSkull;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.FakePlayerFactory;
 
 /**
  * Gany's Nether
@@ -15,11 +23,30 @@ public class DispenserBehaviorSceptreOfWithering extends BehaviorDefaultDispense
 
 	@Override
 	public ItemStack dispenseStack(IBlockSource block, ItemStack stack) {
+		EnumFacing enumfacing = BlockDispenser.func_149937_b(block.getBlockMetadata());
+		IPosition iposition = BlockDispenser.func_149939_a(block);
+		World world = block.getWorld();
+
+		double x = iposition.getX() + enumfacing.getFrontOffsetX() * 0.3F;
+		double y = iposition.getY() + enumfacing.getFrontOffsetX() * 0.3F;
+		double z = iposition.getZ() + enumfacing.getFrontOffsetZ() * 0.3F;
+
+		EntityWitherSkull witherskull = new EntityWitherSkull(world, x, y, z, enumfacing.getFrontOffsetX(), enumfacing.getFrontOffsetY(), enumfacing.getFrontOffsetZ());
+		witherskull.setInvulnerable(true);
+		world.spawnEntityInWorld(witherskull);
+		damageStack(world, stack);
+
 		return stack;
 	}
 
 	@Override
 	protected void playDispenseSound(IBlockSource block) {
-		block.getWorld().playAuxSFX(1009, block.getXInt(), block.getYInt(), block.getZInt(), 0);
+		block.getWorld().playAuxSFX(1014, block.getXInt(), block.getYInt(), block.getZInt(), 0);
+	}
+
+	private void damageStack(World world, ItemStack stack) {
+		EntityPlayer player = FakePlayerFactory.getMinecraft((WorldServer) world);
+		player.setCurrentItemOrArmor(0, stack);
+		stack.damageItem(1, player);
 	}
 }
--- src/main/java/ganymedes01/ganysnether/items/SceptreOfWithering.java
+++ src/main/java/ganymedes01/ganysnether/items/SceptreOfWithering.java
@@ -4,7 +4,10 @@ import ganymedes01.ganysnether.GanysNether;
 import ganymedes01.ganysnether.core.utils.Utils;
 import ganymedes01.ganysnether.lib.Strings;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityWitherSkull;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 /**
@@ -25,6 +28,31 @@ public class SceptreOfWithering extends Sceptre {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		world.playAuxSFXAtEntity(player, 1014, (int) player.posX, (int) player.posY, (int) player.posZ, 0);
+
+		if (!world.isRemote) {
+			Vec3 targetPos = player.getLook(1.0F);
+			EntityWitherSkull witherskull = new EntityWitherSkull(world, player, 0, 0, 0);
+			witherskull.setInvulnerable(true);
+
+			witherskull.posX = player.posX + targetPos.xCoord;
+			witherskull.posY = player.posY + targetPos.yCoord + 1.0D;
+			witherskull.posZ = player.posZ + targetPos.zCoord;
+			double factor = MathHelper.sqrt_double(targetPos.xCoord * targetPos.xCoord + targetPos.yCoord * targetPos.yCoord + targetPos.zCoord * targetPos.zCoord);
+			witherskull.accelerationX = targetPos.xCoord / factor * 0.1D;
+			witherskull.accelerationY = targetPos.yCoord / factor * 0.1D;
+			witherskull.accelerationZ = targetPos.zCoord / factor * 0.1D;
+
+			world.spawnEntityInWorld(witherskull);
+		} else
+			player.swingItem();
+
+		if (!player.capabilities.isCreativeMode) {
+			stack.damageItem(1, player);
+			if (stack.stackSize <= 0)
+				player.setCurrentItemOrArmor(0, null);
+		}
+
 		return stack;
 	}
 }
