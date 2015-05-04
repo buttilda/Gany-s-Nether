777c523c5a439ce83fdd73d74af7075c01389b09
ganymedes01 <foka_12@hotmail.com>
2015-05-04 02:50:04 -0300

    Only integrate with MFR with crops are enabled...

--- src/main/java/ganymedes01/ganysnether/integration/MFRManager.java
+++ src/main/java/ganymedes01/ganysnether/integration/MFRManager.java
@@ -1,5 +1,6 @@
 package ganymedes01.ganysnether.integration;
 
+import ganymedes01.ganysnether.GanysNether;
 import ganymedes01.ganysnether.ModBlocks;
 import ganymedes01.ganysnether.ModItems;
 import ganymedes01.ganysnether.core.utils.Utils;
@@ -35,72 +36,74 @@ public class MFRManager extends Integration {
 
 	@Override
 	public void init() {
-		FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.ghostSeeds, ModBlocks.spectreWheat));
-		FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.quarzBerrySeeds, ModBlocks.quarzBerryBush));
-		FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.hellBushSeeds, ModBlocks.hellBush));
-		FactoryRegistry.sendMessage("registerPlantable", new PlantableSugarCane(Item.getItemFromBlock(ModBlocks.blazingCactoid), ModBlocks.blazingCactoid));
-		FactoryRegistry.sendMessage("registerPlantable", new PlantableSugarCane(ModItems.glowingReed, ModBlocks.glowingReed));
-		FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.witherShrubSeeds, ModBlocks.witherShrub));
-		FactoryRegistry.sendMessage("registerPlantable", new Plantable(Items.ghast_tear, ModBlocks.weepingPod) {
+		if (GanysNether.shouldGenerateCrops) {
+			FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.ghostSeeds, ModBlocks.spectreWheat));
+			FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.quarzBerrySeeds, ModBlocks.quarzBerryBush));
+			FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.hellBushSeeds, ModBlocks.hellBush));
+			FactoryRegistry.sendMessage("registerPlantable", new PlantableSugarCane(Item.getItemFromBlock(ModBlocks.blazingCactoid), ModBlocks.blazingCactoid));
+			FactoryRegistry.sendMessage("registerPlantable", new PlantableSugarCane(ModItems.glowingReed, ModBlocks.glowingReed));
+			FactoryRegistry.sendMessage("registerPlantable", new Plantable(ModItems.witherShrubSeeds, ModBlocks.witherShrub));
+			FactoryRegistry.sendMessage("registerPlantable", new Plantable(Items.ghast_tear, ModBlocks.weepingPod) {
 
-			@Override
-			public boolean canBePlantedHere(World world, int x, int y, int z, ItemStack stack) {
-				for (ForgeDirection side : SIDES)
-					if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == Blocks.obsidian)
-						return world.getBlock(x, y, z).isReplaceable(world, x, y, z);
-				return false;
-			}
+				@Override
+				public boolean canBePlantedHere(World world, int x, int y, int z, ItemStack stack) {
+					for (ForgeDirection side : SIDES)
+						if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == Blocks.obsidian)
+							return world.getBlock(x, y, z).isReplaceable(world, x, y, z);
+					return false;
+				}
 
-			@Override
-			public ReplacementBlock getPlantedBlock(World world, int x, int y, int z, ItemStack stack) {
-				ReplacementBlock block = super.getPlantedBlock(world, x, y, z, stack);
-				for (ForgeDirection side : SIDES)
-					if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == Blocks.obsidian)
-						switch (side.getOpposite().ordinal()) {
-							case 2:
-								block.setMeta(0);
-								break;
-							case 3:
-								block.setMeta(2);
-								break;
-							case 4:
-								block.setMeta(3);
-								break;
-							case 5:
-								block.setMeta(1);
-								break;
-							default:
-								break;
+				@Override
+				public ReplacementBlock getPlantedBlock(World world, int x, int y, int z, ItemStack stack) {
+					ReplacementBlock block = super.getPlantedBlock(world, x, y, z, stack);
+					for (ForgeDirection side : SIDES)
+						if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == Blocks.obsidian)
+							switch (side.getOpposite().ordinal()) {
+								case 2:
+									block.setMeta(0);
+									break;
+								case 3:
+									block.setMeta(2);
+									break;
+								case 4:
+									block.setMeta(3);
+									break;
+								case 5:
+									block.setMeta(1);
+									break;
+								default:
+									break;
+							}
+
+					return block;
+				}
+			});
+
+			FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.spectreWheat));
+			FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.quarzBerryBush));
+			FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.hellBush));
+			FactoryRegistry.sendMessage("registerHarvestable", new HarvestableSugarCane(ModBlocks.blazingCactoid));
+			FactoryRegistry.sendMessage("registerHarvestable", new HarvestableSugarCane(ModBlocks.glowingReed));
+
+			FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(Blocks.skull) {
+				@Override
+				public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z) {
+					for (ForgeDirection side : SIDES)
+						if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == ModBlocks.witherShrub) {
+							TileEntitySkull tile = Utils.getTileEntity(world, x, y, z, TileEntitySkull.class);
+							return tile != null && tile.func_145904_a() == 1;
 						}
+					return false;
+				}
+			});
 
-				return block;
-			}
-		});
-
-		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.spectreWheat));
-		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.quarzBerryBush));
-		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.hellBush));
-		FactoryRegistry.sendMessage("registerHarvestable", new HarvestableSugarCane(ModBlocks.blazingCactoid));
-		FactoryRegistry.sendMessage("registerHarvestable", new HarvestableSugarCane(ModBlocks.glowingReed));
-
-		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(Blocks.skull) {
-			@Override
-			public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z) {
-				for (ForgeDirection side : SIDES)
-					if (world.getBlock(x + side.offsetX, y, z + side.offsetZ) == ModBlocks.witherShrub) {
-						TileEntitySkull tile = Utils.getTileEntity(world, x, y, z, TileEntitySkull.class);
-						return tile != null && tile.func_145904_a() == 1;
-					}
-				return false;
-			}
-		});
-
-		FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.weepingPod) {
-			@Override
-			public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z) {
-				return (world.getBlockMetadata(x, y, z) & 12) >> 2 >= 2;
-			}
-		});
+			FactoryRegistry.sendMessage("registerHarvestable", new Harvestable(ModBlocks.weepingPod) {
+				@Override
+				public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z) {
+					return (world.getBlockMetadata(x, y, z) & 12) >> 2 >= 2;
+				}
+			});
+		}
 	}
 
 	@Override
