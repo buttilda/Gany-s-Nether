18fbf6b43c12972d1f42da79426b5d079ca10c27
ganymedes01 <foka_12@hotmail.com>
2015-06-13 16:20:40 -0300

    Use latest recommended forge

--- src/main/java/ganymedes01/ganysnether/lib/Reference.java
+++ src/main/java/ganymedes01/ganysnether/lib/Reference.java
@@ -11,7 +11,7 @@ public class Reference {
 
 	public static final String MOD_ID = "ganysnether";
 	public static final String MOD_NAME = "Gany's Nether";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.3.1360,);";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,);";
 	public static final String CHANNEL = "GanysNether";
 	public static final String MASTER = "GanysMods";
 	public static final String VERSION_NUMBER = "1.8.0";
